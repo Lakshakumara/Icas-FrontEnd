ffbd0b3bb9063f256393be367488bcb897f1aa99
Lakshakumara <lakshakumara@gmail.com>
2024-04-29 16:32:32 +0530

    2024/04/29

--- src/app/pop/opd/opd.component.ts
+++ src/app/pop/opd/opd.component.ts
@@ -121,6 +121,7 @@ export class OpdComponent implements OnInit {
           allowOutsideClick: () => false,
           preConfirm: async () => {
             try {
+              if(result.value == undefined) throw ''
               let claimid : number = result.value;
               let response: any = await this.auth.downloadClaim(claimid);
               console.log('received from backend ', response);
